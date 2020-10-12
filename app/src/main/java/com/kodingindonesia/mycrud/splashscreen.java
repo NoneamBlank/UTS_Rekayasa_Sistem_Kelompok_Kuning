fd3b6ad3c6ad1ff56f3e0f9b2fc33366b7621ef9
Derwin Galen <derwingalen@gmail.com>
2020-10-12 16:55:39 +0700

    forth commit

---
+++ app/src/main/java/com/kodingindonesia/mycrud/splashscreen.java
@@ -0,0 +1,28 @@
+package com.kodingindonesia.mycrud;
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashscreen extends AppCompatActivity {
+    private int waktu_loading=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(splashscreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+    }
+}
